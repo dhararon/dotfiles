06ffc7f66c657b0cfcd25de8a4ca6dfb408cb925
Dharwin Perez <dhararon@gmail.com>
2024-10-14 10:47:02 -0600

    feat: Add sign commits gitlab

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -13,3 +13,14 @@ zoxide init fish | source
 starship init fish | source
 
 alias cd="z"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    and not set -q TMUX
+
+    and set -q SSH_TTY
+    tmux attach-session -t ssh_tmux; or tmux new-session -s ssh_tmux
+  end
+
+# Sign commits git gitlab
+export GPG_TTY=$(tty)
