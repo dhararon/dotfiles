bd311bf07f9e5aadaa85b46a62dee8e617c13ed3
Dharwin Perez <dharwin@getfairplay.com>
2025-05-11 00:24:46 -0600

    feat(settings): Update settings for ditfiles

--- dotfiles/fish/conf.d/pyenv.fish
+++
@@ -1,6 +0,0 @@
-# -----------------------------------------------------
-# Pyenv configuration
-# -----------------------------------------------------
-set -gx PATH /usr/local/bin:/usr/bin:/bin $PATH
-pyenv init - | source
-status --is-interactive; and pyenv virtualenv-init - | source
--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -3,37 +3,19 @@
 export GPG_TTY=$(tty)
 export TERRAGRUNT_TFPATH=tofu
 
-if status is-interactive
-  cd $HOME
-end
-
 # cargo
 set -gx PATH $HOME/.cargo/bin $PATH
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
-
-# tmux
-if type -q tmux
-    if not test -n "$TMUX"
-        tmux attach-session -t default; or tmux new-session -s default
-    end
-end
+set --export PATH "$HOME/.local/bin" $PATH
 
 # Generated for envman. Do not edit.
 test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
 
 # Alias
-alias cd="z"
 alias tg="terragrunt"
-alias tgi="terragrunt init"
-alias tgp="terragrunt plan"
-alias cn="changie new"
-alias cb="changie batch"
-alias cba="changie batch auto"
-alias cm="changie merge"
-alias gpt="git push --tags"
 
 # RUN SCRIPTS
 zoxide init fish | source
